f3045ac6ed756b73c2cf54cdf43877345a883856
Lukáš Wagner <lukaswagner1@gmail.com>
2021-08-23 18:47:30 +0200

    Fix setup script

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@
 
 from snxvpnversion import VERSION
 
-from distutils.core import setup, Extension
+from setuptools import setup
+
 license = 'BSD License'
 
 description = []
@@ -31,33 +32,35 @@ for line in f :
     description.append (line)
 f.close ()
 
-setup \
-    ( name             = "snxvpn"
-    , py_modules       = ['snxconnect', 'snxvpnversion']
-    , version          = VERSION
-    , description      =
-        "Command-line utility to connect to a Checkpoint SSL VPN "
-    , long_description = ''.join (description)
-    , license          = license
-    , author           = "Ralf Schlatterbeck"
-    , author_email     = "rsc@runtux.com"
-    , platforms        = 'Linux'
-    , url              = "https://github.com/schlatterbeck/snxvpn"
-    , scripts          = ['snxconnect']
-    , install_requires = [ 'bs4', 'pycrypto', 'lxml', 'rsa' ]
-    , classifiers      = \
-        [ 'Development Status :: 3 - Alpha'
-        , 'License :: OSI Approved :: ' + license
-        , 'Operating System :: POSIX :: Linux'
-        , 'Programming Language :: Python'
-        , 'Intended Audience :: Developers'
-        , 'Intended Audience :: Science/Research'
-        , 'Intended Audience :: Information Technology'
-        , 'Intended Audience :: System Administrators'
-        , 'Programming Language :: Python :: 2.7'
-        , 'Programming Language :: Python :: 3.4'
-        , 'Programming Language :: Python :: 3.5'
-        , 'Programming Language :: Python :: 3.6'
+setup(
+    name             = "snxvpn",
+    version          = VERSION,
+    description      = "Command-line utility to connect to a Checkpoint SSL VPN ",
+    long_description = ''.join (description),
+    license          = license,
+    author           = "Ralf Schlatterbeck",
+    author_email     = "rsc@runtux.com",
+    platforms        = 'Linux',
+    url              = "https://github.com/schlatterbeck/snxvpn",
+    py_modules       = ['snxconnect', 'snxvpnversion'],
+    scripts          = ['snxconnect'],
+    install_requires = [
+        'bs4',
+        'pycrypto',
+        'lxml',
+        'rsa'
+        ],
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: ' + license,
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
         ]
     )
-
